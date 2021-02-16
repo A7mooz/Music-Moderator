ec442c51b85e7d62636ad48ee7a7ba33fb1e07c9
A7mooz <57980136+A7mooz@users.noreply.github.com>
2021-02-16 23:02:58 +0300

    fix

--- commands/misc/ping.js
+++ commands/misc/ping.js
@@ -3,7 +3,7 @@ module.exports = {
     callback: (message, arguments, text, client) => {
         const pinging = message.guild.emojis.cache.find(e => e.id == '811279125116485692')
 
-        message.reply(`**${pinging} Calculating ping...**`).then(msg => {
+        message.channel.send(`**${pinging} Calculating ping...**`).then(msg => {
             const ping = msg.createdTimestamp - message.createdTimestamp
 
             msg.edit(`Bot latency: \`${ping}ms\`, API Latency: \`${client.ws.ping}ms\``)
