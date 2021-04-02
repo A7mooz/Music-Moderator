b897a59d76a90bbd6083527b96a8a347ae1c965b
A7mooz <57980136+A7mooz@users.noreply.github.com>
2021-04-02 23:34:21 +0300

    moderate command

---
+++ commands/moderation/moderate.js
@@ -0,0 +1,29 @@
+module.exports = {
+    commands: ['moderate'],
+    permissions: ['MANAGE_NICKNAMES'],
+    guildOnly: true,
+    modOnly: true,
+    callback: ({ message, args, guild }) => {
+
+        const user = message.mentions.users.first() || args[0]
+        const member = guild.member(user)
+
+        if (!user) return message.reply('Please mention a user to moderate their nickname')
+        if (!member) return message.reply("Can't find that member in this guild!")
+
+        function moderate(length) {
+            var result = ''
+            var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+            for (var i = 0; i < length; i++) {
+                result += characters.charAt(Math.floor(Math.random() * characters.length))
+            }
+
+            return result
+        }
+
+        message.reply('Changed!')
+        member.setNickname(`Moderated Nickname ${moderate(9)}`)
+
+    }
+}
